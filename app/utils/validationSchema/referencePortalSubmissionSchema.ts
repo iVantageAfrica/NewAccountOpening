88a99b90c97efec340b8725baf0d14330b848f3a
deenVantage <nurudeen.faniyi@ivantage.africa>
2026-07-30 10:37:17 +0100

    Referee Validation

--- app/utils/validationSchema/referencePortalSubmissionSchema.ts
+++ app/utils/validationSchema/referencePortalSubmissionSchema.ts
@@ -10,19 +10,19 @@ const fileSchema = (label: string) =>
 
 export const referencePortalSubmissionSchema = z.object({
     name: z.string().min(1, "Name is required").max(50, "Name is too long"),
-    email: z.string().email("Invalid email for Referee 1").max(100, "Email is too long"),
+    email: z.string().email("Invalid email for Referee 1").max(70, "Email is too long"),
     mobile: z.string().regex(/^\+?\d{8,15}$/, "Invalid phone number").max(15, "Phone Number is too long"),
-    bankName: z.string().min(1, "Bank Name is required").max(100, "Bank Name is too long"),
-    accountName: z.string().min(1, "Account Name is required").max(100, "Account Name is too long"),
+    bankName: z.string().min(1, "Bank Name is required").max(80, "Bank Name is too long"),
+    accountName: z.string().min(1, "Account Name is required").max(50, "Account Name is too long"),
     accountType: z.string().min(1, "Account Type is required").max(20, "Account Type is too long"),
     signature: fileSchema("Signature is required"),
-    address:z.string().min(1, "Address is required").max(200, "Address is too long"),
+    address:z.string().min(1, "Address is required").max(100, "Address is too long"),
     knownPeriod: z.string().min(1, "Known Period is required").max(50, "Known Period is too long"),
     comment: z.string().min(1, "Comment is required").max(1000, "Comment is too long"),
     accountNumber: z.string().min(1, "Account Number is required").max(10, "Invalid Account Number"),
     accountHolderNumber: z.string().min(1, "Account Holder Number is required").max(10, "Invalid Account Number"),
-    accountHolderName: z.string().min(1, "Account Holder Name is required").max(100, "Account HolderName is too long"),
-    accountHolderEmailAddress: z.string().max(100, "Account Holder Email Address is too long").optional(),
+    accountHolderName: z.string().min(1, "Account Holder Name is required").max(50, "Account HolderName is too long"),
+    accountHolderEmailAddress: z.string().max(70, "Account Holder Email Address is too long").optional(),
 })
 
 
